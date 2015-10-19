42ee188a3cc5d02bf0db45f3574d9a1de73f9365
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-10-19 14:00:37 +0900

    add routing for test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'top#index'
+  get  '/top'               =>    'top#index'
+  get  '/proto'             =>    'proto#index'
+  get  '/proto/new'         =>    'proto#new'
+  get  '/user'              =>    'user#index'
+  get  '/user/new'          =>    'user#new'
+  get  '/user/edit'         =>    'user#edit'
+  get  '/login'             =>    'login#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
