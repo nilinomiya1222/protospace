0f07d0039213f3dc9b1dd421a85ec1be815d9f45
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-10-22 18:11:15 +0900

    change user controller action index to show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :top, only:[:index]
   resources :prototype, only: [:index, :new]
-  resources :user, only: [:index, :new, :edit]
+  resources :user, only: [:show, :new, :edit]
   resources :login, only: [:login]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
