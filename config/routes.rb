7c82a50bc5e106fa7ca7f32fa6b930276afab372
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-10-23 20:55:37 +0900

    add routing users edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :top, only:[:index]
   resources :prototype, only: [:index, :new]
-  resources :user, only: [:show]
+  resources :users, only: [:show, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
