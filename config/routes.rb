cfb09049518b4434eb78d57865078efb0a72efa4
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-10-23 10:09:33 +0900

    delete needless routing for installing devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :top, only:[:index]
   resources :prototype, only: [:index, :new]
-  resources :user, only: [:show, :new, :edit]
-  resources :login, only: [:login]
+  resources :user, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
