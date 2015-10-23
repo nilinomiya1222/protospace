b6dec495b44ea1a5691a513b6dd521d355e7cc0b
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-10-23 10:04:18 +0900

    add column "name","group","works"
    アバターは画像アップロード用gem導入後に追加

--- db/migrate/20151023010027_devise_create_users.rb
+++ db/migrate/20151023010027_devise_create_users.rb
@@ -4,6 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      ## User profile
+      t.string :name,              null: false, default: ""
+      t.string :group,              null: false, default: ""
+      t.string :works,              null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
