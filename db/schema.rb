96e78f7f4a4216c2dccdeae9f680ca8b2bc3b094
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-10-26 13:04:46 +0900

    add column profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023010027) do
+ActiveRecord::Schema.define(version: 20151026030951) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20151023010027) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "profile",                limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
