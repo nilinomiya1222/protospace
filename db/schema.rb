68664b76a91fce4335edd4ff02779166795734cb
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-01 17:20:42 +0900

    proto_id,statusにカラムを変更したマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101043826) do
+ActiveRecord::Schema.define(version: 20151101070029) do
 
   create_table "images", force: :cascade do |t|
-    t.string   "image",      limit: 255
-    t.integer  "proto_id",   limit: 4
-    t.string   "type",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "image",        limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "prototype_id", limit: 4
+    t.string   "status",       limit: 255
   end
 
   create_table "prototypes", force: :cascade do |t|
