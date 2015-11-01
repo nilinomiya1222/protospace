1287798194d09a00f6fea23ca2e0898c652e96c8
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-01 17:25:35 +0900

    ・showアクションの実装 ・carrierwaveに応じたコントローラーへの記述 ・paramsにimagesテーブルに保存したいカラムを追加 ・redirect_toのindexアクションのコントローラーをprototypesに変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,19 +3,24 @@ class PrototypesController < ApplicationController
     @prototypes = Prototype.includes(:user).order("created_at DESC").page(params[:page]).per(8)
   end
 
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
   def new
     @prototype = Prototype.new
+    @prototype.images.build
   end
 
   def create
-    Prototype.create(name: prototype_params[:name], image:prototype_params[:image], copy: prototype_params[:copy], concept: prototype_params[:concept], user_id: current_user.id)
-    redirect_to controller: :top, action: :index
+    Prototype.create(prototype_params)
+    redirect_to controller: :prototypes, action: :index
   end
 
   private
 
   def prototype_params
-    params.require(:prototype).permit(:name, :image, :copy, :concept, :user_id)
+    params.require(:prototype).permit(:name, :copy, :concept, images_attributes: [:image, :status, :prototype_id]).merge(user_id: current_user.id)
   end
 
 end
