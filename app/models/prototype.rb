e84597ba97e03b6d474e22066c5436e3985c5e6f
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-05 00:37:54 +0900

    prototypeのイメージ保存用の処理を記述

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,5 +4,19 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :images
 
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, reject_if: proc { |attributes| attributes['image'].blank? }
+
+  #Default Paginatiton
+  paginates_per 8
+
+  def create_images(image_params)
+    image_params.each do |key, value|
+      if key == "main"
+        images.create(image: value, status: 0)
+      else
+        images.create(image: value, status: 1)
+      end
+    end
+  end
+
 end
