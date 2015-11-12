c350ea77e7857069aefec07415741d65203c28b8
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-13 01:40:35 +0900

    コメント機能の導入

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @prototype = Prototype.find(create_params[:prototype_id])
+    @prototype.comments.create(text: create_params[:text], prototype_id: create_params[:prototype_id], user_id: current_user.id )
+    redirect_to :root
+  end
+
+  private
+  def create_params
+    params.require(:comment).permit(:text, :prototype_id)
+  end
 end
