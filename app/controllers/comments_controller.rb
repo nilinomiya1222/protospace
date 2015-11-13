5c089952c93b3481def33d2b5b811ee1d2624a68
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-13 14:58:21 +0900

    Ajax適用を想定してredirectを削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
   def create
     @prototype = Prototype.find(create_params[:prototype_id])
     @prototype.comments.create(text: create_params[:text], prototype_id: create_params[:prototype_id], user_id: current_user.id )
-    redirect_to :root
   end
 
   private
