8b30e85e2fee73f8c5818e6082f382be27d93500
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-14 11:04:40 +0900

    paramsとそれに伴う記述の変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @prototype = Prototype.find(create_params[:prototype_id])
-    @prototype.comments.create(text: create_params[:text], prototype_id: create_params[:prototype_id], user_id: current_user.id )
+    @prototype.comments.create(create_params)
   end
 
   private
   def create_params
-    params.require(:comment).permit(:text, :prototype_id)
+    params.require(:comment).permit(:text, :prototype_id).merge(user_id: current_user.id)
   end
 end
