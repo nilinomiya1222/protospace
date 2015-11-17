85022d63293a69de3295a38d0e89406f4dddeb0d
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-17 21:37:11 +0900

    create、destroyアクションを定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,11 @@
 class LikesController < ApplicationController
+  def create
+    Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @likes = Like.where(prototype_id: params[:prototype_id])
+  end
+
+  def destroy
+    @like = Like.find_by(user_id: current_user.id)
+    @like.destroy
+  end
 end
