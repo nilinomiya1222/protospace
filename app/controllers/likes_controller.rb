7cae35022504b4da19d48f0e56bc4d4af1d143da
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-18 13:29:27 +0900

    ajaxなしで実装用に記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,12 @@ class LikesController < ApplicationController
   def create
     current_user.likes.create(prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
+    redirect_to :root
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, prototype_id: params[:id])
     @like.destroy
+    redirect_to :root
   end
 end
