e633183d0b8dc92c2e893314b3e4cf13714d4899
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-18 13:25:05 +0900

    リファクタリング

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,6 @@
 class LikesController < ApplicationController
   def create
-    Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    current_user.likes.create(prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
   end
 
