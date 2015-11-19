0cd156f41e92c858776e6373ff8dd443cb9c87fb
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-19 12:47:51 +0900

    ajaxの導入

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,11 @@
 class LikesController < ApplicationController
   def create
+    @prototype = Prototype.find(params[:prototype_id])
     current_user.likes.create(prototype_id: params[:prototype_id])
-    @likes = Like.where(prototype_id: params[:prototype_id])
-    redirect_to :root
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, prototype_id: params[:id])
-    @like.destroy
-    redirect_to :root
+    Like.find_by(user_id: current_user.id, prototype_id: params[:id]).destroy
+    @prototype = Prototype.find(params[:prototype_id])
   end
 end
