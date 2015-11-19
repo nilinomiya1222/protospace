d094616a860be37210c3e7681d779d77d314ff7d
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-19 22:59:39 +0900

    条件分岐をメソッド化

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -25,7 +25,7 @@ class Prototype < ActiveRecord::Base
     end
   end
 
-  def like_user(prototype,user)
-    Like.exists?(prototype_id: prototype.id, user_id: user.id)
+  def like_blank(current_user)
+    likes.find_by(user_id: current_user.id).blank?
   end
 end
