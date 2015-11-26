1dce4cc39219c34ffde8b816f1dbda1ebe2bff49
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-26 18:19:17 +0900

    アップロードした画像のプレビューを表示

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -1,7 +1,64 @@
-$(window).on('load', function() {
+$(function(){
+  add_form();
+  preview_main_form();
+  preview_new_sub_form();
+  preview_edit_sub_form();
+});
+
+function add_form() {
   $('li.plus').on('click', function() {
     $('li.hidden_form').removeClass('hidden_form');
     $(this).addClass('hidden_form');
   });
-});
+};
+
+function preview_main_form() {
+  $('#prototype_images_attributes_0_main').change(
+      function() {
+          if ( !this.files.length ) {
+              return;
+          }
+          var file = $(this).prop('files')[0];
+          var fr = new FileReader();
+          fr.onload = function() {
+              $('.main_image_preview').attr('src', fr.result ).css('display','inline');
+          }
+          fr.readAsDataURL(file);
+      }
+  );
+};
+
+function preview_new_sub_form() {
+  for(var i = 0; i <= 2; i++){
+    $("#prototype_images_attributes_0_sub"+ i).change(function(){
+      if(!this.files.length){
+        return;
+      }
+      var num = $(this).attr("id").charAt($(this).attr("id").search(/[1-9]/));
+      var file = $(this).prop('files')[0];
+      var fr = new FileReader();
+      fr.onload = function(){
+        $('.sub_image' + num + '_preview' ).attr('src', fr.result).css('display', 'inline');
+      }
+      fr.readAsDataURL(file);
+    });
+  }
+}
+
+function preview_edit_sub_form() {
+  for(var i = 0; i <= 2; i++){
+    $("#prototype_images_attributes_"+i+"_sub").change(function(){
+      if(!this.files.length){
+        return;
+      }
+      var num = $(this).attr("id").charAt($(this).attr("id").search(/[0-9]/));
+      var file = $(this).prop('files')[0];
+      var fr = new FileReader();
+      fr.onload = function(){
+        $('.sub_image' + num + '_preview' ).attr('src', fr.result).css('display', 'inline');
+      }
+      fr.readAsDataURL(file);
+    });
+  }
+}
 
