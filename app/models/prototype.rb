90c764b55ffb3698837d5004602bc221e34d1da8
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-27 20:58:10 +0900

    不要なメソッドを削除

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -13,24 +13,6 @@ class Prototype < ActiveRecord::Base
   #Default Paginatiton
   paginates_per 8
 
-  def create_images(image_params)
-    image_params.each do |key, value|
-      key == 'main' ? images.create(image: value, status: 'main') : images.create(image: value, status: 'sub')
-    end
-  end
-
-  def update_images(image_params)
-    i = 0
-    image_params.each do |key, value|
-      if key != '0'
-          images[i].update(image: value[:sub], status: 'sub')
-      else
-        images[0].update(image: value[:main], status: 'main')
-        images.create(image: value[:sub], status: 'sub') if value.has_key?(:sub)
-      end
-      i += 1
-    end
-  end
 
   def like_blank?(current_user)
     likes.find_by(user_id: current_user.id).blank?
