b8f847e0312d09af1e12dfcb06e4811ec49e7cc4
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-27 16:44:50 +0900

    let,const使用のためブロックを定義

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -12,35 +12,39 @@ function addForm() {
 };
 
 function previewMainForm() {
+  "use strict"
   $('#prototype_images_attributes_0_content').change(
-      function() {
-          if ( !this.files.length ) {
-              return;
-          }
-          let file = $(this).prop('files')[0];
-          const FileReader = new FileReader();
-          FileReader.onload = function() {
-              $('.main_image_preview').attr('src', FileReader.result ).css('display','inline');
-          }
-          FileReader.readAsDataURL(file);
+    function() {
+      if ( !this.files.length ) {
+          return;
+      }else{
+      let file = $(this).prop('files')[0];
+      const FILEREADER = new FileReader();
+        FILEREADER.onload = function() {
+          $('.main_image_preview').attr('src', FILEREADER.result ).css('display','inline');
+        }
+      FILEREADER.readAsDataURL(file);
       }
+    }
   );
 };
 
 function previewSubForm() {
+  "use strict";
   for(let i = 1; i <= 3; i++){
     $('#prototype_images_attributes_' + i + '_content').change(function(){
       if(!this.files.length){
         return;
-      }
-      let num = $(this).attr('id').charAt($(this).attr('id').search(/[1-9]/));
-      let file = $(this).prop('files')[0];
-      const FileReader = new FileReader();
-      FileReader.onload = function(){
-        $('.sub_image' + num + '_preview' ).attr('src', FileReader.result).css('display', 'inline');
-      }
-      FileReader.readAsDataURL(file);
-    });
+      }else{
+        let num = $(this).attr('id').charAt($(this).attr('id').search(/[1-9]/));
+        let file = $(this).prop('files')[0];
+        const FILEREADER = new FileReader();
+        FILEREADER.onload = function(){
+          $('.sub_image' + num + '_preview' ).attr('src', FILEREADER.result).css('display', 'inline');
+        }
+        FILEREADER.readAsDataURL(file);
+      };
+    })
   }
 }
 
