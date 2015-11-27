5ffff251c566783272a281143d8853950344189f
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-27 12:55:38 +0900

    カラム名変更に伴う記述の変更

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   #Association
   belongs_to :prototype
 
-  mount_uploader :image, ImageUploader
+  mount_uploader :content, ImageUploader
 
   #Main,Subの区別
   enum status: %i(main sub)
