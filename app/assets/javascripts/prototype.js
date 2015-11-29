4a0bb23eff8dccf55eeb66271d607b1e29beca25
Tomoki Ninomiya <nilinomiya@gmail.com>
2015-11-29 21:15:50 +0900

    メソッドの統合

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -1,8 +1,7 @@
 "use strict";
 $(function(){
   addForm();
-  previewMainForm();
-  previewSubForm();
+  previewForm();
 });
 
 function addForm() {
@@ -12,34 +11,17 @@ function addForm() {
   });
 };
 
-function previewMainForm() {
-  $('#prototype_thumbnails_attributes_0_content').change(
-    function() {
-      if ( !this.files.length ) {
-          return;
-      }else{
-      let file = $(this).prop('files')[0];
-      const fileReader = new FileReader();
-        fileReader.onload = function() {
-          $('.main_image_preview').attr('src', fileReader.result ).css('display','inline');
-        }
-      fileReader.readAsDataURL(file);
-      }
-    }
-  );
-};
-
-function previewSubForm() {
-  for(let i = 1; i <= 3; i++){
+function previewForm() {
+  for(let i = 0; i <= 3; i++){
     $('#prototype_thumbnails_attributes_' + i + '_content').change(function(){
       if(!this.files.length){
         return;
       }else{
-        let num = $(this).attr('id').charAt($(this).attr('id').search(/[1-9]/));
+        let num = $(this).attr('id').charAt($(this).attr('id').search(/[0-9]/));
         let file = $(this).prop('files')[0];
         const fileReader = new FileReader();
         fileReader.onload = function(){
-          $('.sub_image' + num + '_preview' ).attr('src', fileReader.result).css('display', 'inline');
+          $('.image' + num + '_preview' ).attr('src', fileReader.result).css('display', 'inline');
         }
         fileReader.readAsDataURL(file);
       };
